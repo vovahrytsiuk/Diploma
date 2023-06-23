4074fceb63555e6041012b119bc5e4248bd15699
Volodymyr Hrytsiuk <volodymyr_hrytsiuk@epam.com>
2023-06-23 10:19:20 +0300

    change file path

--- Components/Parser/include/Parser/Parser.h
+++ Components/Parser/include/Parser/Parser.h
@@ -18,7 +18,7 @@ public:
     std::string getError() { return error.getError(); }
 
 private:
-    const std::string pathFile{"../CppUI.txt"};
+    const std::string pathFile{"CppUI.txt"};
 
     std::ifstream fin;
 
